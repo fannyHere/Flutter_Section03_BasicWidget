18b9d3113a326e43c1f50f35f337612db84baa1d
unknown <Ratubilkis8936@gmail.com>
2024-02-15 15:11:45 +0700

    Section 03 - 02 Widget Dasar

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        //bagian atas scaffold
-        backgroundColor: Color.fromARGB(255, 110, 10, 82),
-        title: Text("coba dulu"),
-        centerTitle: true, //menengahkan text mau apa si
-      ),
-      //backgroundColor: Color.fromARGB(255, 230, 159, 212),
+      home: Scaffold(
+          appBar: AppBar(
+            //bagian atas scaffold
+            backgroundColor: Color.fromARGB(255, 110, 10, 82),
+            title: Text("coba dulu"),
+            centerTitle: true, //menengahkan text mau apa si
+          ),
+          //backgroundColor: Color.fromARGB(255, 230, 159, 212),
+
+          body: Center(
+              //Image Provider:
+
+              //1. Asset Image -> gambar yang ada pada folder project dan perlu daftar di pubspecyaml
+              //cara lama:
+              //   child: Image(
+              // image: AssetImage("assetsName/emong.jpg"),
+              //cara cepat:
+              //child: Image.asset("assetsName/emong.jpg"),
+
+              //2. Network Image -> ambil dari internet dan oneksi butuh internet
+              //cara lama:
+              // child: Image(
+              //     image: NetworkImage(
+              //         "https://linkimage.com/cdn/shop/products/SesseNightLightsIII4a_Formatmall500x700_700x.jpg?v=1650395661")),
+              //cara cepat:
+              // child: Image.network(
+              //     "https://linkimage.com/cdn/shop/products/SesseNightLightsIII4a_Formatmall500x700_700x.jpg?v=1650395661"),
+
+              //Jarang digunakan: hanya pada kasus tertentu
+              //3. File images
+              //4. Memory images
+              )),
 
       //latihan ikon data
-
-      body: Center(
-          child: Icon(
-        Icons.home,
-        size: 200,
-        color: Colors.purple.shade100,
-      )),
+      // body: Center(
+      //     child: Icon(
+      //   Icons.home,
+      //   size: 200,
+      //   color: Colors.purple.shade100,
+      // )), //center //scaffold
 
       //latihan button klik
       // body: Center(
@@ -35,15 +58,15 @@ class MyApp extends StatelessWidget {
       //     print("KLIK");
       //   },
       //   child: Text("KLIK SAYA"),
-      // )
-      // )
+      // ) //center
+      // ) //scaffold
 
       //latihan flutter logo
       // body: Center(
       //   child: FlutterLogo(
       //     size: 200,
-      //   ),
-      // )
+      //   ), //center
+      // ) //scaffold
 
       //latihan hello world
       //   Center(
@@ -58,8 +81,8 @@ class MyApp extends StatelessWidget {
       //     ),
       //   )
       //       //child: Text("Hello Fanny Jelek"),
-      //       ),
-      // ),
-    ));
+      //       ), //center
+      // ), //scaffold
+    );
   } //widget build
 }//stateless widget
